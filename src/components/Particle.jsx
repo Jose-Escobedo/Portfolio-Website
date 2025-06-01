776b01baa79fda74623cd018d52bdc8fef1e7dda
Jose Escobedo <escobedo.jose48@outlook.com>
2025-05-31 17:25:14 -0700

    edirting partcilclees

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -5,19 +5,21 @@ const Particle = () => {
   const particlesInit = async (main) => {
     await loadFull(main);
   };
-  const particlesLoaded = (container) => {};
+
   return (
     <Particles
       id="tsparticles"
       init={particlesInit}
-      loaded={particlesLoaded}
       options={{
+        fullScreen: {
+          enable: false, 
+        },
         particles: {
           number: {
-            value: 355,
+            value: 80, 
             density: {
               enable: true,
-              value_area: 789.1476416322727,
+              value_area: 600,
             },
           },
           color: {
@@ -25,98 +27,41 @@ const Particle = () => {
           },
           shape: {
             type: "circle",
-            stroke: {
-              width: 0,
-              color: "#000000",
-            },
-            polygon: {
-              nb_sides: 5,
-            },
-            image: {
-              src: "img/github.svg",
-              width: 100,
-              height: 100,
-            },
           },
           opacity: {
-            value: 0.48927153781200905,
-            random: false,
+            value: 0.3,
             anim: {
-              enable: true,
-              speed: 0.2,
-              opacity_min: 0,
-              sync: false,
+              enable: false,
             },
           },
           size: {
             value: 2,
             random: true,
-            anim: {
-              enable: true,
-              speed: 2,
-              size_min: 0,
-              sync: false,
-            },
           },
           line_linked: {
             enable: false,
-            distance: 150,
-            color: "#ffffff",
-            opacity: 0.4,
-            width: 1,
           },
           move: {
             enable: true,
-            speed: 0.2,
+            speed: 0.3,
             direction: "none",
             random: true,
             straight: false,
             out_mode: "out",
-            bounce: false,
-            attract: {
-              enable: false,
-              rotateX: 600,
-              rotateY: 1200,
-            },
           },
         },
         interactivity: {
           detect_on: "canvas",
           events: {
             onhover: {
-              enable: true,
+              enable: false,
               mode: "bubble",
             },
             onclick: {
-              enable: true,
+              enable: false,
               mode: "push",
             },
             resize: true,
-          },
-          modes: {
-            grab: {
-              distance: 400,
-              line_linked: {
-                opacity: 1,
-              },
-            },
-            bubble: {
-              distance: 83.91608391608392,
-              size: 1,
-              duration: 3,
-              opacity: 1,
-              speed: 3,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-            push: {
-              particles_nb: 4,
-            },
-            remove: {
-              particles_nb: 2,
-            },
           },
         },
         retina_detect: true,
